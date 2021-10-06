6963d78c4ace06f67828eb4f24b9616198cedc68
theBeginner86 <pranavsingh02@hotmail.com>
2021-10-06 09:58:44 +0530

    Update signin.js with AS working

--- client/src/components/signin/signin.js
+++ client/src/components/signin/signin.js
@@ -1,16 +1,18 @@
 import React, {useState} from 'react';
 import {useHistory} from 'react-router-dom'
 import {signinExistingUser} from "../../api/userApi";
+import decryptMessages from "../../utils/decryptMessages";
+import encryptMessages from "../../utils/encryptMessages";
 import './signin.css'
 
-function SignIn(props) {
+function SignIn() {
 
-    console.log(props);
-    const {setIsLogout, isLogout} = props;
-    console.log(isLogout);
+    // console.log(props);
+    // const {setIsLogout, isLogout} = props;
+    // console.log(isLogout);
     const history = useHistory();
 
-    const [email, setEmail] = useState("");
+    const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
     const [signinResponse, setSigninResponse] = useState("");
@@ -19,22 +21,52 @@ function SignIn(props) {
         event.preventDefault();
 
         const userData = {
-            email,
+            username,
             password
         };
 
         try {
-            const {data} = await signinExistingUser(userData);
-            console.log("data: ", data)
+            const { data } = await signinExistingUser(userData);
+            console.log("data: ", data);
             setSigninResponse(data);
-            console.log("signinResponse: ", signinResponse);
+            // console.log("signinResponse: ", signinResponse);
+            
+            const encryptedClientMessage = data.encryptedClientMessage;
+            console.log(encryptedClientMessage);
+            const clientSecretKey = data.clientSecretKey;  
+            console.log(clientSecretKey);
+            const encryptedTGT = data.encryptedTGT;
+            console.log(encryptedTGT);
+            const { TGS_ID, TGS_SessionKey, timestamp } = decryptMessages(encryptedClientMessage, clientSecretKey);
+            // console.log(TGS_ID, timestamp, TGS_SessionKey);
+
+            // console.log(TGS_SessionKey);
+            const timeElapsed = Date.now();
+            const todaysDate = new Date(timeElapsed);
+
+            const userCredentials = {
+                username,
+                timeStamp: todaysDate.toUTCString()
+            };
+
+            const encryptedUserCredentails = encryptMessages(userCredentials, TGS_SessionKey);
+
+            const messageForTGS = {
+                encryptedTGT,
+                encryptedUserCredentails
+            };
+            
+            console.log(messageForTGS);
+
             if(data.success){
-                setIsLogout(false);
-                localStorage.setItem("token", data.accessToken);
+                // setIsLogout(false);
+                // localStorage.setItem("token", data.accessToken);
                 console.log(history);
                 history.push("/");
             }
         } catch(err) {
+            console.log("inside");
+            console.log(err);
             setSigninResponse({message: err});
         }
     }
@@ -50,12 +82,12 @@ function SignIn(props) {
                     </div>
                         <form onSubmit={handleSubmit} className="signin-form">
                             <div className="input-values">
-                                {
+                                {/* {
                                     (!signinResponse.success) ?  <div className="err-msg"><div className="msg">{signinResponse.message}</div></div> : <div className="err-msg"></div>
-                                }
+                                } */}
                                 <div className="input">
-                                    <input type="email" name="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/><br/> 
-                                    <input type="password" name="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}/><br/>
+                                    <input type="text" name="username" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} /><br/> 
+                                    <input type="password" name="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} /><br/>
                                 </div>
                             </div>
                             <div className="submit-btn" Style={"margin-top: 1rem"}>
